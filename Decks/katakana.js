95c1e8df069c6902f36316f27d50684e042152e6
Luís Pinto <Luspin@microsoft.com>
2023-08-29 16:17:11 +0100

    Added Katakana Deck

---
+++ Decks/katakana.js
@@ -0,0 +1,362 @@
+[
+  {
+    "question"  : "ア",
+    "answer"    : "a",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "イ",
+    "answer"    : "i",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ウ",
+    "answer"    : "u",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "エ",
+    "answer"    : "e",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "オ",
+    "answer"    : "o",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "カ",
+    "answer"    : "ka",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "キ",
+    "answer"    : "ki",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ク",
+    "answer"    : "ku",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ケ",
+    "answer"    : "ke",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "コ",
+    "answer"    : "ko",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "サ",
+    "answer"    : "sa",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "シ",
+    "answer"    : "shi",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ス",
+    "answer"    : "su",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "セ",
+    "answer"    : "se",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ソ",
+    "answer"    : "so",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "タ",
+    "answer"    : "ta",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "チ",
+    "answer"    : "chi",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ツ",
+    "answer"    : "tsu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "テ",
+    "answer"    : "te",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ト",
+    "answer"    : "to",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ナ",
+    "answer"    : "na",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ニ",
+    "answer"    : "ni",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヌ",
+    "answer"    : "nu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ネ",
+    "answer"    : "ne",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ノ",
+    "answer"    : "no",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ハ",
+    "answer"    : "ha",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヒ",
+    "answer"    : "hi",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "フ",
+    "answer"    : "fu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヘ",
+    "answer"    : "he",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ホ",
+    "answer"    : "ho",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "マ",
+    "answer"    : "ma",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ミ",
+    "answer"    : "mi",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ム",
+    "answer"    : "mu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "メ",
+    "answer"    : "me",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "モ",
+    "answer"    : "mo",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヤ",
+    "answer"    : "ya",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ユ",
+    "answer"    : "yu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヨ",
+    "answer"    : "yo",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ラ",
+    "answer"    : "ra",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "リ",
+    "answer"    : "ri",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ル",
+    "answer"    : "ru",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "レ",
+    "answer"    : "re",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ロ",
+    "answer"    : "ro",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ワ",
+    "answer"    : "wa",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヲ",
+    "answer"    : "wo",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ン",
+    "answer"    : "n",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ガ",
+    "answer"    : "ga",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ギ",
+    "answer"    : "gi",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "グ",
+    "answer"    : "gu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ゲ",
+    "answer"    : "ge",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ゴ",
+    "answer"    : "go",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ザ",
+    "answer"    : "za",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ジ",
+    "answer"    : "ji",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ズ",
+    "answer"    : "zu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ゼ",
+    "answer"    : "ze",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ゾ",
+    "answer"    : "zo",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ダ",
+    "answer"    : "da",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヂ",
+    "answer"    : "ji",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヅ",
+    "answer"    : "zu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "デ",
+    "answer"    : "de",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ド",
+    "answer"    : "do",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "バ",
+    "answer"    : "ba",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ビ",
+    "answer"    : "bi",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ブ",
+    "answer"    : "bu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ベ",
+    "answer"    : "be",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ボ",
+    "answer"    : "bo",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "パ",
+    "answer"    : "pa",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ピ",
+    "answer"    : "pi",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "プ",
+    "answer"    : "pu",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ペ",
+    "answer"    : "pe",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ポ",
+    "answer"    : "po",
+    "seenTimes" : 0
+  },
+  {
+    "question"  : "ヴ",
+    "answer"    : "vu",
+    "seenTimes" : 0
+  }
+]
